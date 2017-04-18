18d692bfd313de39ce4a7c04b4f455b9fb71069c
Steven Nienhuis <steven.nienhuis@atos.net>
2017-04-18 18:20:16 +0200

    nieuwe step def toegevoegd

--- src/test/java/com/atos/cucumberdemo/step/adactinSteps.java
+++ src/test/java/com/atos/cucumberdemo/step/adactinSteps.java
@@ -42,6 +42,12 @@ public class adactinSteps {
         webDriver.get("http://adactin.com/HotelAppBuild2/");
     }
 
+    @Given("^I am on the adactin site with current user$")
+    public void I_am_on_the_adactin_site_with_current_user() throws Throwable {
+        webDriver.manage().window().maximize();
+        webDriver.get("http://adactin.com/HotelAppBuild2/");
+    }
+
     @When("^I log in with my credentials$")
     public void I_log_in_with_my_credentials() throws Throwable {
         webDriver.findElement(By.id("username")).sendKeys("TestlabCucumberDemo");
